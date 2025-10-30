4634c5e77f4ca0b523e6c8c69971dc4a8f90033e
Boyle, Wesley <wesleyboyle@my.unt.edu>
2025-10-29 19:19:46 -0500

    Created state variable and list of all pages(states) in the app

--- Flutter/knitting_crochet_repository/lib/main.dart
+++ Flutter/knitting_crochet_repository/lib/main.dart
@@ -21,3 +21,30 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+class _HomeState extends State<Home> {
+  // Tracks which page is selected
+  int _selectedIndex = 0;
+
+  //List of all pages
+  final List<Widget> _pages = [
+    WelcomePage(),
+    LoginPage(),
+    HomePage(),
+    SearchPage(),
+    UserSettingPage(),
+    MyProfilePage(),
+    AccessibilityPage(),
+    PrivacySettingsPage(),
+    SavedPostsPage(),
+    MakePostPage(),
+    AccessPostPage(),
+    PopularPage(),
+    CategoriesPage(),
+    EnlargedInstructionsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
