fd2c43f27ec6a7d435a7a36a3a16a769c8718633
Boyle, Wesley <wesleyboyle@my.unt.edu>
2025-11-29 15:07:39 -0600

    Rebase to main

--- Flutter/knitting_crochet_repository/lib/main.dart
+++ Flutter/knitting_crochet_repository/lib/main.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+/*
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import './postFunctions.dart';
 import './authFunctions.dart';
+*/
 
 void main() async {
   /*WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  */
   runApp(const MyApp());
 }
  
