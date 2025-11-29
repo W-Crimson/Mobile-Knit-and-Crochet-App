ac9a836767572f83ee4f242191ed060518300b47
Boyle, Wesley <wesleyboyle@my.unt.edu>
2025-11-28 20:17:01 -0600

    Added Dependencies:

--- Flutter/knitting_crochet_repository/lib/main.dart
+++ Flutter/knitting_crochet_repository/lib/main.dart
@@ -1,17 +1,17 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import './postFunctions.dart';
-import './authFunctions.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  /*WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  );*/
   runApp(const MyApp());
 }
-
+ 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
